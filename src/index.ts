d16a01f17462168d924980ef8858f3cecf61e5f4
Konrad <vobis08gw@gmail.com>
2025-06-16 08:01:06 +0200

    added swagger

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,23 @@ import { logger } from './utils/logger';
 const app = express();
 const PORT = config.port;
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerJSDoc = require('swagger-jsdoc');
+
+const swaggerOptions = {
+  swaggerDefinition: {
+    info: {
+      title: 'Leave Me API',
+      version: '1.0.0',
+      description: 'Leave Me API Documentation',
+    },
+  },
+  apis: ['./src/routes/*.ts'],
+};
+
+const swaggerSpec = swaggerJSDoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
 app.use(express.json());
 
 app.use('/api/test', testRouter);
--- src/routes/test.route.ts
+++ src/routes/test.route.ts
@@ -3,6 +3,26 @@ import * as testController from "../controllers/test.controller";
 
 const router = Router();
 
+/**
+ * @swagger
+ * /api/test:
+ *   get:
+ *     summary: Get test data
+ *     description: Retrieve test data from the server
+ *     responses:
+ *       200:
+ *         description: Successful response
+ *         content:
+ *           application/json:
+ *             example:
+ *               message: Test data retrieved successfully
+ *               data:
+ *                 - id: 1
+ *                   name: Test 1
+ *                 - id: 2
+ *                   name: Test 2
+ */
+
 router.get("/", testController.getTest);
 
 export default router;
