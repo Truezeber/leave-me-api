44be8b9b743f1981ae984656a3255572a49b5f0c
Konrad <vobis08gw@gmail.com>
2025-06-20 14:20:54 +0200

    Update auth.utils.ts

--- src/utils/auth.utils.ts
+++ src/utils/auth.utils.ts
@@ -1,6 +1,8 @@
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
-import { config } from "dotenv";
+import { config } from "../config/app.config";
+import { JwtPayload } from "../models/auth.models";
+import { randomBytes } from "crypto";
 
 export const auth = {
   async hashPassword(password: string): Promise<string> {
@@ -8,5 +10,16 @@ export const auth = {
   },
   async comparePassword(password: string, hash: string): Promise<boolean> {
     return bcrypt.compare(password, hash);
+  },
+  generateJwt(payload: JwtPayload): string {
+    return jwt.sign(payload, config.jwtSecret as string, {
+      expiresIn: "1h",
+    });
+  },
+  verifyJwt(token: string): JwtPayload {
+    return jwt.verify(token, config.jwtSecret as string) as JwtPayload;
+  },
+  generateRefreshToken(): string {
+    return randomBytes(64).toString("hex");
   },
 };
