c9c3da37859e8ba5fe2c1bdc2603df05f58d037e
Konrad <vobis08gw@gmail.com>
2025-06-20 09:54:01 +0200

    LeaveMeID validator tests

--- src/__tests__/utils.validators.test.ts
+++ src/__tests__/utils.validators.test.ts
@@ -23,3 +23,39 @@ describe("E-mail validator", () => {
 });
 
 describe("LeaveMeId validator", () => {
+  test("Valid LeaveMeId", () => {
+    expect(validator.leaveMeId("@thisisid")).toBe(true);
+  });
+
+  test("Valid LeaveMeId with special character", () => {
+    expect(validator.leaveMeId("@thisisid#")).toBe(true);
+  });
+
+  test("Valid LeaveMeId with number", () => {
+    expect(validator.leaveMeId("@thisisid1")).toBe(true);
+  });
+
+  test("Valid LeaveMeId with upper case letters", () => {
+    expect(validator.leaveMeId("@ThisisID")).toBe(true);
+  });
+
+  test("Valid LeaveMeId with combined characters", () => {
+    expect(validator.leaveMeId("@ThisisID#1")).toBe(true);
+  });
+
+  test("No @", () => {
+    expect(validator.leaveMeId("ThisIsID#1")).toBe(false);
+  });
+
+  test("@ at the end instead", () => {
+    expect(validator.leaveMeId("ThisIsID#1@")).toBe(false);
+  });
+
+  test("Too short", () => {
+    expect(validator.leaveMeId("@Th")).toBe(false);
+  });
+
+  test("Too long", () => {
+    expect(validator.leaveMeId("@ThisIsTooLongID#1")).toBe(false);
+  });
+});
