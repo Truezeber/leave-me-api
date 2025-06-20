f148206b6c310c722e8002cb313672528a3dd243
Konrad <vobis08gw@gmail.com>
2025-06-20 09:42:34 +0200

    email validator test

---
+++ src/__tests__/utils.validators.test.ts
@@ -0,0 +1,23 @@
+import { validator } from "../utils/validators";
+
+describe("E-mail valudator", () => {
+  test("Valid e-mail", () => {
+    expect(validator.email("test@test.com")).toBe(true);
+  });
+
+  test("No domain", () => {
+    expect(validator.email("test@test")).toBe(false);
+  });
+
+  test("No username", () => {
+    expect(validator.email("@test.com")).toBe(false);
+  });
+
+  test("No domain & username", () => {
+    expect(validator.email("@test")).toBe(false);
+  });
+
+  test("No @", () => {
+    expect(validator.email("testtest.com")).toBe(false);
+  });
+});
