3a643b9f3d7a54e43ba4de3a1078e78ad2f16d39
Konrad <vobis08gw@gmail.com>
2025-06-20 10:32:20 +0200

    implemented validation

--- src/services/register.service.ts
+++ src/services/register.service.ts
@@ -1,18 +1,44 @@
-import { client, mainDb } from '../config/database.config';
-import { User, UserRegister } from '../models/user.model';
-import { logger } from '../utils/logger';
+import { client, mainDb } from "../config/database.config";
+import { User, UserRegister } from "../models/user.model";
+import { logger } from "../utils/logger";
+import { validator } from "../utils/validators";
 
 export const registerUser = async (user: UserRegister): Promise<User> => {
   try {
     if (!client) {
-      logger.warn('Database client is not available');
-      throw new Error('Database client is not available');
+      logger.warn("Database client is not available");
+      throw new Error("Database client is not available");
     }
 
-    const collection = mainDb.collection('users');
-    logger.info('Mongo collection', collection);
+    if (!validator.email(user.email)) {
+      logger.warn("Invalid email");
+      throw new Error("Invalid email");
+    }
 
-    logger.info('User:', user);
+    if (!validator.password(user.password)) {
+      logger.warn("Invalid password");
+      throw new Error("Invalid password");
+    }
+
+    if (!validator.nickname(user.nickname)) {
+      logger.warn("Invalid nickname");
+      throw new Error("Invalid nickname");
+    }
+
+    if (!validator.url(user.avatar_url)) {
+      logger.warn("Invalid avatar url");
+      throw new Error("Invalid avatar url");
+    }
+
+    if (!validator.leaveMeId(user.leave_me_id)) {
+      logger.warn("Invalid LeaveMeId");
+      throw new Error("Invalid LeaveMeId");
+    }
+
+    const collection = mainDb.collection("users");
+    logger.info("Mongo collection", collection);
+
+    logger.info("User:", user);
 
     const newUser: User = {
       email: user.email,
@@ -33,27 +59,27 @@ export const registerUser = async (user: UserRegister): Promise<User> => {
       points: 0,
       is_banned: false,
       is_admin: false,
-      join_date: new Date()
+      join_date: new Date(),
     };
-    logger.info('Registering user:', newUser);
+    logger.info("Registering user:", newUser);
 
     const result = await collection.insertOne(newUser);
 
     if (!result.acknowledged) {
-        logger.error('Failed to insert user');
-        throw new Error('Failed to insert user');
+      logger.error("Failed to insert user");
+      throw new Error("Failed to insert user");
     }
     logger.info(`User registered successfully with ID: ${result.insertedId}`);
 
     const createdUser = await collection.findOne({ _id: result.insertedId });
 
     if (!createdUser) {
-      throw new Error('User was created but could not be retrieved');
+      throw new Error("User was created but could not be retrieved");
     }
-    
+
     return createdUser as unknown as User;
   } catch (error) {
-    logger.error('Error registering user:', error);
+    logger.error("Error registering user:", error);
     throw error;
   }
 };
