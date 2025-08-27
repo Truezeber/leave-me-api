d34c2d0aaec21a8c27c77eeb14d0ead53c5344fb
Zeber <vobis08gw@gmail.com>
2025-08-27 21:09:42 +0200

    docs: swagger for /friends/invite

---
+++ src/routes/friends.route.ts
@@ -0,0 +1,39 @@
+import { Router } from "express";
+import * as friendsController from "../controllers/friends.controller"
+import { handleAuth } from "../middlewares/auth.middleware.js";
+const router = Router();
+/**
+ * @swagger
+ *
+ * /friends/invite:
+ *   post:
+ *     summary: Sends an invite to a friend.
+ *     tags: [Friends]
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - in: body
+ *         name: body
+ *         schema:
+ *           type: object
+ *           properties:
+ *             friendLid:
+ *               type: string
+ *     responses:
+ *       200:
+ *         description: Invite sent successfully
+ *       403:
+ *         description: User is blocked or blocking the target user
+ *       404:
+ *         description: Target user not found
+ *       409:
+ *         descritpion: Alreagy friends, invite already sent or recieved invite from this user
+ *       500:
+ *         description: Internal server error
+ *       503:
+ *         description: Database error
+ */
+
+router.post("/invite", handleAuth, friendsController.inviteFriend);
+
+export default router;
