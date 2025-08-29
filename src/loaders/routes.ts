e6e3fc234b9def6891584931bf2fbdad687afd41
Zeber <vobis08gw@gmail.com>
2025-08-29 23:37:34 +0200

    feat: blocks endpoints mounted

--- src/loaders/routes.ts
+++ src/loaders/routes.ts
@@ -3,10 +3,12 @@ import registerRouter from "../routes/register.route";
 import loginRouter from "../routes/login.route";
 import logoutRouter from "../routes/logout.route";
 import friendsRouter from "../routes/friends.route";
+import blockRouter from "../routes/block.route"
 
 export const setupRoutes = (app: Application): void => {
   app.use("/api/register", registerRouter);
   app.use("/api/login", loginRouter);
   app.use("/api/logout", logoutRouter);
   app.use("/api/friends", friendsRouter);
+  app.use("/api/block", blockRouter);
 };
