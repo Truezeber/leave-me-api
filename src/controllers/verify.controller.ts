9d8648eb616d5e6032a1b0eda1c0bb36b2c69508
Zeber <vobis08gw@gmail.com>
2025-08-31 18:40:15 +0200

    feat: confirmPin controller

--- src/controllers/verify.controller.ts
+++ src/controllers/verify.controller.ts
@@ -48,3 +48,26 @@ export const requestNewPin = async (
     })
   }
 }
+
+export const confirmPin = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    logger.info("POST /api/v1/auth/request-new-pin - Sending new PIN e-mail");
+
+    await verifyService.confirmPin(req.body.email, req.body.pin);
+
+    res
+      .status(200)
+      .json({
+        message: "PIN verified"
+      });
+
+  } catch (error: any) {
+    const status = error.statusCode || 500;
+    res.status(status).json({
+      error: error.message || "Something went wrong",
+    })
+  }
+}
