27407d915ce4cc15ff701e0797470e422f1cfa50
Zeber <vobis08gw@gmail.com>
2025-09-02 11:05:11 +0200

    feat: createPost route

---
+++ src/routes/posts.route.ts
@@ -0,0 +1,39 @@
+import { Router } from "express";
+import * as postsController from "../controllers/posts.controller";
+import { handleAuth } from "../middlewares/auth.middleware";
+const router = Router();
+/**
+ * @swagger
+ *
+ * /api/v1/posts/create:
+ *   post:
+ *     summary: Creates a post.
+ *     tags: [Posts]
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - in: body
+ *         name: body
+ *         schema:
+ *           type: object
+ *           properties:
+ *             origin:
+ *               type: string
+ *             content:
+ *               type: string
+ *     responses:
+ *       200:
+ *         description: Invite sent successfully
+ *       403:
+ *         description: User is blocked or blocking the target user
+ *       404:
+ *         description: Target user not found
+ *       409:
+ *         descritpion: Already friends, invite already sent or recieved invite from this user
+ *       500:
+ *         description: Internal server error
+ *       503:
+ *         description: Database error
+ */
+
+router.post("/create", handleAuth, postsController.sendPost);
