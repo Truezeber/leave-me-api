8cf03fb769091dfa5db9881ff46961196ae355cb
Zeber <vobis08gw@gmail.com>
2025-09-02 23:58:11 +0200

    feat: deletePost controller

--- src/controllers/posts.controller.ts
+++ src/controllers/posts.controller.ts
@@ -33,3 +33,35 @@ export const sendPost = async (
     })
   }
 }
+
+export const deletePost = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    logger.info("POST /api/v1/posts/delete - Deleting a post");
+
+    let [userLid, origin] = [
+      (req as any).user,
+      req.body.origin,
+    ];
+
+    if (typeof origin === "string" && origin.length === 24 && /^[a-f0-9]+$/i.test(origin)) {
+      origin = new ObjectId(origin);
+    } else {
+      throw { message: "Origin is not a valid ObjectId", statusCode: 400 }
+    }
+
+    const response = await postsService.deletePost(userLid, origin);
+
+    res
+      .status(200)
+      .json(response);
+
+  } catch (error: any) {
+    const status = error.statusCode || 500;
+    res.status(status).json({
+      error: error.message || "Something went wrong",
+    })
+  }
+}
