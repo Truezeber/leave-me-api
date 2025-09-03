edf884c9a6fabee0deb8873823b1b88cdd6c0d3a
Zeber <vobis08gw@gmail.com>
2025-09-03 17:04:23 +0200

    feat: getUser endpoint

---
+++ src/routes/users.route.ts
@@ -0,0 +1,37 @@
+import { Router } from "express";
+import * as usersController from "../controllers/users.controller";
+import { handleAuth } from "../middlewares/auth.middleware";
+const router = Router();
+
+/**
+ * @swagger
+ *
+ * /api/v1/users/get-user:
+ *   get:
+ *     summary: Loads user profile info.
+ *     tags: [Users]
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - in: query
+ *         name: target
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: Target leave me id
+ *     responses:
+ *       200:
+ *         description: User loaded successfully
+ *       400:
+ *         description: Invalid request parameters
+ *       404:
+ *         description: User not found
+ *       500:
+ *         description: Internal server error
+ *       503:
+ *         description: Database error
+ */
+
+router.get("/get-user", handleAuth, usersController.getUser);
+
+export default router;
