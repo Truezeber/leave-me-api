4e285e9b3005b6f0b3c697932d3bfca5f43a0835
Zeber <vobis08gw@gmail.com>
2025-09-04 11:02:20 +0200

    feat: refresh route registered

--- src/loaders/routes.ts
+++ src/loaders/routes.ts
@@ -2,6 +2,7 @@ import { Application } from "express";
 import registerRouter from "../routes/register.route";
 import loginRouter from "../routes/login.route";
 import logoutRouter from "../routes/logout.route";
+import refreshRouter from "../routes/refresh.route";
 import friendsRouter from "../routes/friends.route";
 import blockRouter from "../routes/block.route";
 import verifyRouter from "../routes/verify.route";
@@ -13,6 +14,7 @@ export const setupRoutes = (app: Application): void => {
   app.use("/api/v1/auth", registerRouter);
   app.use("/api/v1/auth", loginRouter);
   app.use("/api/v1/auth", logoutRouter);
+  app.use("/api/v1/auth", refreshRouter);
   app.use("/api/v1/friends", friendsRouter);
   app.use("/api/v1/block", blockRouter);
   app.use("/api/v1/auth", verifyRouter);
