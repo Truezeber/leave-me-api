713d3a9e7ebb30c85ae779bec7535e2e0710d7bf
Zeber <vobis08gw@gmail.com>
2025-09-04 10:59:06 +0200

    feat: refresh controller

---
+++ src/controllers/refresh.controller.ts
@@ -0,0 +1,33 @@
+import { Request, Response } from "express";
+import * as refreshService from "../services/refresh.service";
+import { logger } from "../utils/logger.utils";
+
+export const refreshToken = async (req: Request, res: Response): Promise<void> => {
+  try {
+    logger.info("POST /api/v1/auth/refresh-token - Refreshing token");
+
+    const refreshToken = req.cookies.refresh_token;
+
+    if (!refreshToken) {
+      logger.warn("No refresh token provided");
+      res.status(401).json({ error: "No refresh token provided" });
+      return;
+    }
+
+    const result = await refreshService.refreshToken(refreshToken);
+
+    res
+      .status(200)
+      .cookie("access_token", result, {
+        httpOnly: true,
+        //secure: true, //! <- uncomment before deploy
+        sameSite: true,
+        maxAge: 1000 * 60 * 60,
+      });
+  } catch (error: any) {
+    logger.error("Error refreshing token:", error);
+    res.status(error.statusCode || 401).json({
+      error: error.message || "Unauthorized",
+    });
+  }
+};
