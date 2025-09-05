5336c7ac3c06fddf35ad65f2d1cef55dc9139e72
Zeber <vobis08gw@gmail.com>
2025-09-05 11:24:18 +0200

    feat: unbanUser service

--- src/services/admin.service.ts
+++ src/services/admin.service.ts
@@ -5,7 +5,7 @@ import { logger } from "../utils/logger.utils";
 export const banUser = async (
   leave_me_id: string,
   target_id: string
-): Promise<string> => { //function props should be sexier
+): Promise<string> => {
   try {
     if (!client) {
       logger.warn("Database client is not available");
@@ -38,4 +38,39 @@ export const banUser = async (
   }
 };
 
+export const unbanUser = async (
+  leave_me_id: string,
+  target_id: string
+): Promise<string> => {
+  try {
+    if (!client) {
+      logger.warn("Database client is not available");
+      throw { message: "Database client is not available", statusCode: 503 };
+    }
+
+    const usersCollection = mainDb.collection<User>("users");
+
+    const adminUser = await usersCollection.findOne({ leave_me_id: leave_me_id }) as User;
+
+    if (!adminUser.is_admin) {
+      throw { message: "You can't do that", statusCode: 403 };
+    }
+
+    const targetUser = await usersCollection.findOne({ leave_me_id: target_id }) as User;
+
+    if (!targetUser) {
+      throw { message: "User not found", statusCode: 404 };
+    }
+
+    if (!targetUser.is_banned) {
+      throw { message: "User is not banned", statusCode: 409 };
+    }
+
+    await usersCollection.updateOne({ leave_me_id: target_id }, { $set: { is_banned: false } });
+    return "Success";
+  } catch (error) {
+    logger.error("Error unbanning a user:", error);
+    throw error;
+  }
+};
 
