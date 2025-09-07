591d89242d82007fc927b1bcfaa0b3bc29d086e0
Zeber <vobis08gw@gmail.com>
2025-09-07 20:04:14 +0200

    refactor: users controller

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { logger } from "../utils/logger.utils";
+import { transformer } from "../utils/transformers.utils";
 
 import * as usersService from "../services/users.service";
 
@@ -10,10 +11,7 @@ export const getUser = async (
   try {
     logger.info("GET /api/v1/users/get - getting a user");
 
-    let [userLid, target] = [
-      (req as any).user,
-      req.query.target as string,
-    ];
+    const [userLid, target] = transformer.toString((req as any).user, req.query.target);
 
     const response = await usersService.getUser(userLid, target);
 
