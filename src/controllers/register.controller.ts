704978de87f16d52fc777b50deb1cfe218a457a9
Zeber <vobis08gw@gmail.com>
2025-09-07 20:45:41 +0200

    refactor register logs & calls

--- src/controllers/register.controller.ts
+++ src/controllers/register.controller.ts
@@ -1,10 +1,37 @@
 import { Request, Response } from "express";
 import * as registerService from "../services/register.service";
 import { logger } from "../utils/logger.utils";
+import { validator } from "../utils/validators.utils";
 
 export const postUser = async (req: Request, res: Response): Promise<void> => {
   try {
     logger.info("POST /api/v1/auth/register - Registering user");
+
+    const user = req.body;
+
+    if (!validator.email(user.email)) {
+      throw { message: "Invalid credentials", statusCode: 400 };
+    }
+
+    if (!validator.password(user.password)) {
+      throw { message: "Invalid credentials", statusCode: 400 };
+    }
+
+    if (!validator.nickname(user.nickname)) {
+      throw { message: "Invalid credentials", statusCode: 400 };
+    }
+
+    if (!validator.url(user.avatar_url)) {
+      throw { message: "Invalid credentials", statusCode: 400 };
+    }
+
+    if (!validator.leaveMeId(user.leave_me_id)) {
+      throw { message: "Invalid credentials", statusCode: 400 };
+    }
+
+    if (!user.pp_accepted || !user.tos_accepted) {
+      throw { message: "Terms Of Service or Privacy Policy must be accepted", statusCode: 422 }
+    }
 
     const [refreshToken, accessToken] = await registerService.registerUser(req.body);
 
--- src/services/register.service.ts
+++ src/services/register.service.ts
@@ -2,7 +2,6 @@ import { client, mainDb } from "../config/database.config";
 import { User, UserConfirmation, UserRegister } from "../models/user.model";
 import { auth } from "../utils/auth.utils";
 import { logger } from "../utils/logger.utils";
-import { validator } from "../utils/validators.utils";
 import { Notifier } from "../models/notifications.model";
 
 export const registerUser = async (user: UserRegister): Promise<string[]> => {
@@ -12,60 +11,24 @@ export const registerUser = async (user: UserRegister): Promise<string[]> => {
       throw { message: "Database client is not available", statusCode: 503 };
     }
 
-    if (!validator.email(user.email)) {
-      logger.warn("Invalid email");
-      throw { message: "Invalid email", statusCode: 400 };
-    }
-
-    if (!validator.password(user.password)) {
-      logger.warn("Invalid password");
-      throw { message: "Invalid password", statusCode: 400 };
-    }
-
-    if (!validator.nickname(user.nickname)) {
-      logger.warn("Invalid nickname");
-      throw { message: "Invalid nickname", statusCode: 400 };
-    }
-
-    if (!validator.url(user.avatar_url)) {
-      logger.warn("Invalid avatar url");
-      throw { message: "Invalid avatar url", statusCode: 400 };
-    }
-
-    if (!validator.leaveMeId(user.leave_me_id)) {
-      logger.warn("Invalid LeaveMeID");
-      throw { message: "Invalid LeaveMeID", statusCode: 400 };
-    }
-
-    if (!user.pp_accepted || !user.tos_accepted) {
-      logger.warn("PP or TOS unaccepted");
-      throw { message: "Terms Of Service or Privacy Policy must be accepted", statusCode: 422 }
-    }
-
     const collection = mainDb.collection<User>("users");
     const confirmationCollection = mainDb.collection<UserConfirmation>("usersConfirmation");
     const notificationsCollection = mainDb.collection<Notifier>("notifications");
+
     const confirmedUser = await confirmationCollection.findOne({ email: user.email });
 
-    logger.info("Mongo collection", collection);
-    logger.debug("ConfirmedUser: ", confirmedUser);
-
     if (await collection.findOne({ email: user.email })) {
-      logger.warn("Email already exists");
-      throw { message: "Email already exists", statusCode: 409 };
+      throw { message: "Existing data problem", statusCode: 409 };
     }
 
     if (await collection.findOne({ leave_me_id: user.leave_me_id })) {
-      logger.warn("LeaveMeID already exists");
-      throw { message: "LeaveMeID already exists", statusCode: 409 };
+      throw { message: "Existing data problem", statusCode: 409 };
     }
 
     if (!confirmedUser || !confirmedUser.verified) {
-      logger.warn("Email not verified");
-      throw { message: "Email not verified", statusCode: 409 };
+      throw { message: "Existing data problem", statusCode: 409 };
     }
 
-    logger.info("User:", user);
 
     const newUser: User = {
       email: user.email,
@@ -94,20 +57,14 @@ export const registerUser = async (user: UserRegister): Promise<string[]> => {
       leave_me_id: user.leave_me_id,
       notifications: []
     }
-    logger.info("Registering user:", newUser);
 
     const result = await collection.insertOne(newUser);
 
     if (!result.acknowledged) {
-      logger.error("Failed to insert user");
-      throw new Error("Failed to insert user");
+      throw new Error("Undefined database problem");
     }
 
     await notificationsCollection.insertOne(newNotifier);
-
-    logger.success(
-      `User registered successfully with ID: ${result.insertedId}`
-    );
 
     const refreshToken = newUser.refresh_tokens[0];
     const accessToken = auth.generateJwt({ leave_me_id: newUser.leave_me_id });
