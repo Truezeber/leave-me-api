85c2bd9841668e8e4b3b8f1aaeff2b16eeab066b
Zeber <vobis08gw@gmail.com>
2025-09-07 18:24:54 +0200

    refactor: register controller

--- src/controllers/register.controller.ts
+++ src/controllers/register.controller.ts
@@ -4,9 +4,10 @@ import { logger } from "../utils/logger.utils";
 
 export const postUser = async (req: Request, res: Response): Promise<void> => {
   try {
-    logger.info("POST /api/register - Registering user");
-    const registerResponse = await registerService.registerUser(req.body);
-    const [refreshToken, accessToken] = registerResponse;
+    logger.info("POST /api/v1/auth/register - Registering user");
+
+    const [refreshToken, accessToken] = await registerService.registerUser(req.body);
+
     res
       .status(200)
       .cookie("refresh_token", refreshToken, {
