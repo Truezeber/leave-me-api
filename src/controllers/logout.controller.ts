38863d21e7e6da6fc00694b4935e5c339160a6f3
Zeber <vobis08gw@gmail.com>
2025-09-07 17:50:18 +0200

    refactor logout controller

--- src/controllers/logout.controller.ts
+++ src/controllers/logout.controller.ts
@@ -1,18 +1,16 @@
 import { Request, Response } from "express";
 import { logger } from "../utils/logger.utils";
 import * as logoutService from "../services/logout.service";
+import { transformer } from "../utils/transformers.utils";
 
 export const logoutUser = async (
   req: Request,
   res: Response
 ): Promise<void> => {
   try {
-    logger.info("POST /api/logout - Logging out user");
+    logger.info("POST /api/v1/auth/logout - Logging out user");
 
-    const [refreshToken, leaveMeId] = [
-      req.cookies.refresh_token,
-      (req as any).user, //* Retarded but who'll stop me
-    ];
+    const [refreshToken, leaveMeId] = transformer.toString(req.cookies.refresh_token, (req as any).user);
 
     logoutService.logoutUser(leaveMeId, refreshToken);
 
@@ -32,6 +30,7 @@ export const logoutUser = async (
         message: "Logout done",
         refresh_token: refreshToken,
       });
+
   } catch (error: any) {
     const status = error.statusCode || 500;
     res.status(status).json({
